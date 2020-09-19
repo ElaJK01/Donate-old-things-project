6a9c8b124104e61cdba66e6987f5e29017907071
Elżbieta Janus-Kaleta <60756867+ElaJK01@users.noreply.github.com>
2020-09-20 00:28:04 +0200

    models

--- GiveAway_project/settings.py
+++ GiveAway_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'donation',
+    'phone_field',
 ]
 
 MIDDLEWARE = [
--- donation/models.py
+++ donation/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from phone_field import PhoneField
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+
+
+class Institution(models.Model):
+    FUNDATION = 1
+    NGO = 2
+    LOCAL_COLLECTION = 3
+
+    TO_WHO_DONNATE = (
+        (FUNDATION, 'Fundation'),
+        (NGO, 'NGO'),
+        (LOCAL_COLLECTION, 'Local collecion'),
+    )
+    name = models.CharField(max_length=250)
+    description = models.TextField()
+    type = models.IntegerField(choices=TO_WHO_DONNATE, default=1)
+    categories = models.ManyToManyField(Category)
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, null=True, on_delete=models.SET_NULL)
+    address = models.CharField(max_length=250)
+    phone_number = PhoneField(blank=True, help_text='Telefon kontaktowy')
+    city = models.CharField(max_length=250)
+    zip_code = models.CharField(max_length=250)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.TextField()
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+
